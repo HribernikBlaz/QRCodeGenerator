cffb4b09a391321fe4e559dbc3c0a3768765d8f0
HribernikBlaz <hrib.blazhrib@gmail.com>
2024-02-02 21:07:45 +0100

    added author and logo

--- js/script.js
+++ js/script.js
@@ -15,10 +15,8 @@ const onGenerateSubmit = (e) => {
     alert("Please enter a URL");
   } else {
       generateQRCode(url, size);
-      //showScanner();
       // Generate the save button after the qr code image src is ready
       setTimeout(() => {
-        // Get save url
         const saveUrl = qr.querySelector("canvas").toDataURL();
         // Create save button
         createSaveBtn(saveUrl);
@@ -47,12 +45,10 @@ const clearUI = () => {
 
 // Create save button to download QR code as image
 const createSaveBtn = (saveUrl) => {
-  // Ustvarite <div> element
   const saveBtnContainer = document.createElement("div");
-  saveBtnContainer.id = "save-button"; // Nastavite id na "save-button"
-  saveBtnContainer.classList = "save-button-container"; // Dodajte morebitne dodatne razrede
+  saveBtnContainer.id = "save-button";
+  saveBtnContainer.classList = "save-button-container";
 
-  // Ustvarite <a> element
   const link = document.createElement("a");
   link.id = "save-link";
   link.classList = "save-button";
@@ -61,15 +57,10 @@ const createSaveBtn = (saveUrl) => {
   link.href = saveUrl;
   link.download = "qrcode.png";
 
-  // Dodajte <a> element v <div> element
   saveBtnContainer.appendChild(link);
 
-  // Dodajte <div> element v ustrezni nadrejeni element (npr. "#generated")
   document.getElementById("generated").appendChild(saveBtnContainer);
 };
-
-
-//hideSpinner();
 
 form.addEventListener("submit", onGenerateSubmit);
 
